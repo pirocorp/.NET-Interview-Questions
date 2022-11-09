db4551dcb02a0d29b53db5b965375e4b80e0b2e2
Zdravko Zdravkov <zdr0@yahoo.com>
2022-11-09 17:29:53 +0100

    04. Dynamic

---
+++ 04. Dynamic/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Dynamic
+{
+    using System;
+    using IronPython.Hosting;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            dynamic variable = "123";
+            
+            variable.MethodNotExists();
+
+            var toUpper = variable.ToUpper();
+
+            var cast = (string) variable;
+
+            Console.WriteLine(RunPython());
+        }
+
+        public static dynamic RunPython()
+        {
+            const string pythonScript =
+            @"class PythonClass:
+            def toUpper(self, input):
+                return input.upper()";
+
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            var operations = engine.Operations;
+
+            engine.Execute(pythonScript, scope);
+            var pythonClassObject = scope.GetVariable("PythonClass");
+            var instance = operations.CreateInstance(pythonClassObject);
+
+            return instance.toUpper("Hello World!");
+        }
+    }
+}
