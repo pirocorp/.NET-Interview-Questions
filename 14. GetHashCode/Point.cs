f71ef23b7ec59a9051e11425f31d4c4404146c22
Zdravko Zdravkov <zdr0@yahoo.com>
2022-11-09 17:32:29 +0100

    14. GetHashCode

---
+++ 14. GetHashCode/Point.cs
@@ -0,0 +1,37 @@
+﻿namespace GetHashCode
+{
+    using System;
+
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        // When overriding the GetHashCode method is important to not forget
+        // override Equals method too
+        public override int GetHashCode()
+        {
+            // Most of the base types in C# already provide a good
+            // implementation of the GetHashCode Method
+            return HashCode.Combine(this.X, this.Y); // Combining two hash-codes into one
+        }
+
+        // Two points are equal when their coordinates are equal
+        public override bool Equals(object? obj)
+        {
+            if (obj is Point point)
+            {
+                return this.X == point.X && this.Y == point.Y;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 14. GetHashCode/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace GetHashCode
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var anyObject1 = "abc";
+            var anyObject2 = 123;
+
+            // The GetHashCode method is a hash function implementation for an object
+            // A hash function is a one-way algorithm that maps an input of any size
+            // to a unique output of a fixed length of bits.
+            Console.WriteLine($"'{anyObject1}' hash code is {anyObject1.GetHashCode()}");
+            Console.WriteLine($"'{anyObject2}' hash code is {anyObject2.GetHashCode()}");
+
+            // Two object considered equal should have the same hash code.
+            // Default implementation for reference types is based on reference (memory address)
+            // For value types is calculated based on the values stored.
+            var point1 = new Point(10, 20);
+            var point2 = new Point(10, 20);
+            var point3 = new Point(20, 20);
+
+            Console.WriteLine($"{point1.GetType().Name} hash code is {point1.GetHashCode()}");
+            Console.WriteLine($"{point2.GetType().Name} hash code is {point2.GetHashCode()}");
+            Console.WriteLine($"{point3.GetType().Name} hash code is {point3.GetHashCode()}");
+        }
+    }
+}
