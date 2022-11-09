d467a317f623694939ded9923273f2de3c73119a
Zdravko Zdravkov <zdr0@yahoo.com>
2022-11-09 17:30:35 +0100

    06. Lambda Expressions

---
+++ 06. Lamda Expressions/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LambdaExpressions
+{
+    public static class Program
+    {
+        private static Func<int, DateTime, string, decimal> someFunc;
+        private static Action<string, string, bool> someAction;
+
+        public static void Main()
+        {
+            var arr = new[] { 1, 2, 3 };
+
+            Func<int, bool> func = n => n % 2 == 0;
+
+            // Lambda Expressions (Anonymous Functions) (p1, p2) => expression
+            IsAny(arr, number => number > 10);
+            IsAny(arr, number => number % 2 == 0);
+        }
+
+        public static bool IsAny(
+            IEnumerable<int> numbers,
+            Predicate<int> predicate)
+        {
+            foreach (var number in numbers)
+            {
+                if (predicate(number))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
