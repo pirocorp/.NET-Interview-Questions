06caf4a17acb7a8e1aff50ea3c73e74fc8e7a3e8
Zdravko Zdravkov <zdr0@yahoo.com>
2022-11-09 17:29:15 +0100

    02. IS vs AS

---
+++ 02. IS vs AS/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IS_vs_AS
+{
+    using System.Collections.Generic;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            object text = "hello";
+
+            bool isString = text is string;
+            bool isInt = text is int;
+
+            // as can be used only with nullable types, because if it fails null will be returned
+
+            string? textAsString = text as string; // hello
+            List<int>? list = text as List<int>; // null
+            // int number = text as int // Invalid null can't be assigned to int
+            List<int> list2 = (List<int>)text; // InvalidCastException
+        }
+    }
+}
