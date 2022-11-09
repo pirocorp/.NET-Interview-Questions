ebbf9413d3d9e90c32b4ba4069072b10def8a4b9
Zdravko Zdravkov <zdr0@yahoo.com>
2022-11-09 17:32:02 +0100

    12. Checked

---
+++ 12. Checked/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Checked
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var result = 0;
+            try
+            {
+                // Enables check for overflow exception
+                checked
+                {
+                    var twoBillion = 2_000_000_000;
+                    result = twoBillion + twoBillion;
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("the result is: " + result);
+        }
+    }
+}
